440955baf47e475f580b4c59df5c41b54c3be503
Mohamed ELMesseiry <m.messeiry@gmail.com>
2018-09-03 13:04:32 +0400

    Add files via upload

---
+++ deletevDisks.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+adminUser="developer"
+adminPass="changeme"
+idracDefaultUser="root"
+idracDefaultPass="calvin"
+publicKeyFile="/home/developer/.ssh/id_rsa.pub"
+userIndex="4"
+list="deletevDisk.list"
+
+for server in `cat $list`;
+do
+    echo "Configuring idrac for server: $server ..."
+    echo "getting the existing vdisks"
+
+	vDiskCmdOutput=`racadm -r $server -u $adminUser -p $adminPass --nocertwarn raid get vdisks`
+	echo "\n\n"
+	echo "--- $server -----------------------------"
+	echo "$vDiskCmdOutput"
+	echo "----------------------------------------------"
+	echo ">> Raid 0 is configured with 2 disks ..."
+	echo ">> deleting virtual disks Disk.Virtual.0:RAID.Integrated.1-1 ...."
+	
+	racadm -r $server -u $adminUser -p $adminPass --nocertwarn storage deletevd:Disk.Virtual.0:RAID.Integrated.1-1
+	racadm -r $server -u $adminUser -p $adminPass --nocertwarn jobqueue  create RAID.Integrated.1-1 -s TIME_NOW --realtime
+	racadm -r $server -u $adminUser -p $adminPass --nocertwarn jobqueue view 
+
+
+
+	echo "\n\n"
+done
---
+++ disableUserPassWarning.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+adminUser="developer"
+adminPass="changeme"
+idracDefaultUser="root"
+idracDefaultPass="calvin"
+publicKeyFile="/home/developer/.ssh/id_rsa.pub"
+userIndex="4"
+
+
+for server in `cat server.list`;
+do
+    echo "Configuring idrac for server: $server ..."
+    echo "getting the existing vdisks"
+
+	racadm -r $server -u $adminUser -p $adminPass --nocertwarn set iDRAC.Tuning.DefaultCredentialWarning Disabled
+	
+
+done
